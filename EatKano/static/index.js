9dedd8defcbc16f40375befd7b75f91b616d935f
Xiaoyu-233 <neonyui95@gmail.com>
2022-01-23 21:52:49 +1100

    change title and submitResults

--- EatKano/static/index.js
+++ EatKano/static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '就这？包子是你爹？';
+    if (score <= 99) return '还行，跟狗包子55开';
+    if (score <= 149) return '牛批，狗包子泪目';
+    if (score <= 199) return '¿';
     return '人？';
 }
 
